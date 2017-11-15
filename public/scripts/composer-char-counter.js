3b4a025e3ee1e7ceccf9bb3dc4a683a765b1d8ed
V. Anastassiou <v.anastassiou@gmail.com>
2017-11-15 01:21:52 +0000

    Implemented red text on incorrect character count

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,6 +2,12 @@ $("document").ready(function() {
   const maxLength = 140;
   $(".new-tweet textarea").on("input", function () {
     const charsLeft = maxLength - $(this).val().length;
-    $(this).parent().find('.counter').text(charsLeft);
+    const counter = $(this).parent().find('.counter');
+    counter.text(charsLeft);
+    if (charsLeft < 0) {
+      counter.addClass("invalid");
+    } else {
+      counter.removeClass("invalid")
+    };
   });
 });
